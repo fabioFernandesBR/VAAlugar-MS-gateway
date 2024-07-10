8f330d630b87c65bda65dbefd4a9154261ce20ca
fabioFernandesBR <fabio.bp.fernandes@gmail.com>
2024-07-10 19:54:51 -0300

    revertendo a mudança da porta

--- suporte/__init__.py
+++ suporte/__init__.py
@@ -1,12 +1,22 @@
 ##
 
 
-
+# Isso funcionava no VS Code
 MICROSERVICE_URLS = {
     'MS-Reservas': 'http://localhost:5001',
     'MS-Canoas': 'http://localhost:5002',
     'MS-Avaliacoes': 'http://localhost:5003'
 }
+
+# Para rodar dentro da Docker (??):
+'''MICROSERVICE_URLS = {
+    'MS-Reservas': 'http://ms-reservas:5001',
+    'MS-Canoas': 'http://ms-canoas-2:5002',
+    'MS-Avaliacoes': 'http://ms-avaliacoes:5003'
+}'''
+
+
+
 
 
 def model_dump(self, **kwargs):
